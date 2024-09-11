69d536067e2d62fa22bd40683e07068d69d58227
Gifsho <padyod6@gmail.com>
2024-09-11 16:02:59 +0700

    YOSSY Update Show Name

--- services/ticket.services.js
+++ services/ticket.services.js
@@ -1,6 +1,7 @@
 const Ticket = require('../model/ticket.model');
 const LottoSer = require('../services/lotto.services')
 const LottoMo = require('../model/lotto.model');
+const userMo = require('../model/user.model');
 
 exports.getAllTicket = async () => {
     return await Ticket.find();
@@ -42,13 +43,18 @@ exports.TgetOne = async (userId) => {
             const ticket = await LottoMo.findOne({ _id: user.LottoID });
             console.log('Wallet fetched:', ticket);
 
+            const nameU = await userMo.findOne({ _id: userId });
+            console.log('xxx:', nameU.name);
+
             return {
+                name: nameU.name,
                 id: user.UserID,
                 ticket: ticket.LottoNumber || {}
             };
         } catch (err) {
             console.error(`Failed to fetch wallet for user ${user.UserID}:`, err);
             return {
+                name: nameU.name,
                 id: user.UserID,
                 ticket: ticket.LottoNumber || {}
             };
