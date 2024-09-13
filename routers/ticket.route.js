6e6d9d8ac8cbad3c26b58f0eebb925f2b34b548e
Gifsho <padyod6@gmail.com>
2024-09-13 21:37:16 +0700

    up

--- routers/ticket.route.js
+++ routers/ticket.route.js
@@ -3,7 +3,7 @@ const TicketController = require('../controller/ticket.controller');
 
 router.get('/ticket', TicketController.getAllTicket);
 router.post('/delT/:userId', TicketController.delT);
-router.delete('/delAllT', TicketController.delAllT);
+router.post('/delAllT', TicketController.delAllT);
 router.get('/ticket/:userId', TicketController.TgetOne);
 router.post('/delTN/:num', TicketController.delTN);
 
