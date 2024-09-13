68e34e1be35c88f1e551248b9b98fe93f0f2ea88
Flurr <65011212162@msu.ac.th>
2024-09-13 21:45:44 +0700

    My Comment to Add File

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const app = require('./app');
-const db = require('./config/db');
-const UserModel = require('./model/user.model');
-const WalletModel = require('./model/wallet.model');
-const winningNumbers = require('./model/winningNumbers.model');
 
-const port = 8081;
+// const port = 8081;
+const port = process.env.PORT || 8081;
 
 app.get('/',(req,res)=>{
     res.send("Hello World!!!!!")
@@ -13,3 +10,9 @@ app.get('/',(req,res)=>{
 app.listen(port,()=>{
     console.log(`Server Listening on Port http://localhost:${port}`)
 });
+
+// จัดการกับ unhandled errors
+process.on('unhandledRejection', (error) => {
+    console.error('Unhandled Rejection:', error);
+    process.exit(1);
+  });
