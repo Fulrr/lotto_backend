55d5b46ba7616051b439e24e80d0768a5d09707c
Fulrr <162682614+Fulrr@users.noreply.github.com>
2024-09-13 16:59:07 +0700

    Update ticket.route.js

--- routers/ticket.route.js
+++ routers/ticket.route.js
@@ -3,6 +3,7 @@ const TicketController = require('../controller/ticket.controller');
 
 router.get('/ticket', TicketController.getAllTicket);
 router.post('/delT/:userId', TicketController.delT);
+router.delete('/delAllT', TicketController.delAllT);
 router.get('/ticket/:userId', TicketController.TgetOne);
 
 module.exports = router;
